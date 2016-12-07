aa74238f06a3c25dbbf26f941437d2fdc570dbaf
Sean Kirmani <sean@kirmani.io>
2016-12-07 15:51:46 -0800

    changed active/passive wait time

--- src/ask_about_object.py
+++ src/ask_about_object.py
@@ -31,7 +31,7 @@ class AskAboutObject(Action):
                 {},
                 {})
         self.active_ = False
-        self.wait_time_ = 2.0 if self.active_ else 4.0
+        self.wait_time_ = 1.0 if self.active_ else 4.0
 
     def Task(self):
         bowl_phrases = [" is empty", " is blue", " can hold things", " can hold food", " can be full", " can hold soup"]
